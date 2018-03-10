bd6dc4b85d6e34aafbb2b3dc17816e0abc7630ae
Karol <swieca.karol@gmail.com>
2018-03-10 17:24:37 +0100

    fix

--- src/domek.js
+++ src/domek.js
@@ -10,23 +10,23 @@ const mailer = require('./mailSender');
 const db = require('./db');
 const logger = require('./logger')
 
-// const gumtreeData = [
-//   {
-//     url: 'https://www.gumtree.pl/s-mieszkania-i-domy-do-wynajecia/krowodrza+gorka/v1c9008q0p1?nr=2&pr=,1600',
-//     keyword: 'Krowodrza Górka, max 1600 PLN, 2 pokoje',
-//     shortkey: 'kgorka'
-//   },
-//   {
-//     url: 'https://www.gumtree.pl/s-mieszkania-i-domy-do-wynajecia/zabiniec/v1c9008q0p1?nr=2&pr=,1600',
-//     keyword: 'Żabiniec, max 1600 PLN, 2 pokoje',
-//     shortkey: 'zabiniec'
-//   },
-//   {
-//     url: 'https://www.gumtree.pl/s-mieszkania-i-domy-do-wynajecia/rondo+mogilskie/v1c9008q0p1?nr=2&pr=,1600',
-//     keyword: 'Mogilskie, max 1600 PLN, 2 pokoje',
-//     shortkey: 'mogilskie'
-//   }
-// ]
+const gumtreeData = [
+  {
+    url: 'https://www.gumtree.pl/s-mieszkania-i-domy-do-wynajecia/krowodrza+gorka/v1c9008q0p1?nr=2&pr=,1600',
+    keyword: 'Krowodrza Górka, max 1600 PLN, 2 pokoje',
+    shortkey: 'kgorka'
+  },
+  {
+    url: 'https://www.gumtree.pl/s-mieszkania-i-domy-do-wynajecia/zabiniec/v1c9008q0p1?nr=2&pr=,1600',
+    keyword: 'Żabiniec, max 1600 PLN, 2 pokoje',
+    shortkey: 'zabiniec'
+  },
+  {
+    url: 'https://www.gumtree.pl/s-mieszkania-i-domy-do-wynajecia/rondo+mogilskie/v1c9008q0p1?nr=2&pr=,1600',
+    keyword: 'Mogilskie, max 1600 PLN, 2 pokoje',
+    shortkey: 'mogilskie'
+  }
+]
 
 const domek = {
   async getGumtreeRequest(flat) {
@@ -90,7 +90,15 @@ module.exports = {
 };
 
 
+var cron = require('node-cron');
 
+cron.schedule('*/10 * * * * *', function () {
+  console.log('running a task every 10 sec');
+  domek.init(gumtreeData)
+});
+module.exports = {
+  domek
+};
 
 
 /* 
